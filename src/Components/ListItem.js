a859399e962327fcd1b96423ef52a10af34bcd5d
Tim254 <tim.mailu@gmail.com>
2022-09-12 02:07:11 +0300

    clean up list item component

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// let getTitle = (note) => {
+//   const title = note.body.split('\n')[0]
+//   if (title.length > 45) {
+//     return title.slice(0, 45)
+//   }
+  
+//   return title
+// }
+
+let getTime = () => {
+  return new Date(note.updated).toLocaleDateString()
+}
+
 const ListItem = ({note}) => {
   return (
     <Link to={`/note/${note.id}`}>
     
      <div className='notes-list-item'>
         
-        <h3>{note.body}</h3>
-        <h5>{note.author}</h5>
+        {/* <h3>{getTitle(note)}</h3> */}
+        <p><span>{note.title}</span></p>
+        <p><span>{getTime(note)}</span></p>
+        <p><span>{note.author}</span></p>
 
     </div>
     </Link>
