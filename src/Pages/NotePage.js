187c1e7dfe8ea56ca99dd41c147e769cf6d0d195
Tim254 <tim.mailu@gmail.com>
2022-09-12 00:33:23 +0300

    create a delete functionality

--- src/Pages/NotePage.js
+++ src/Pages/NotePage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/Arrowleft.svg";
 // import notes from "../assets/data";
 
-function NotePage({ match }) {
+function NotePage({ match , history}) {
   let noteId = match.params.id;
 
   let [note, setNote] = useState(null)
@@ -23,21 +23,45 @@ let updateNote = async () => {
   await fetch(`http://localhost:5000/notes/${noteId}`, {
     method: 'PUT',
     headers: {
-      'Content-Type': 'application',
-      body:JSON.stringify({...note, 'updated'})
-    }
-  })
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...note, 'updated': new Date() })
+})
 }
+
+let deleteNote = async () => {
+  await fetch(`http://localhost:5000/notes/${noteId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(note)
+  })
+  history.push('/')
+}
+
+let handleSubmit = () => {
+  if(noteId != 'new' && !note.body) {
+    deleteNote()
+  }
+  else if (noteId !== 'new') {
+    updateNote()
+  }
+  updateNote()
+  history.push('/')
+}
+
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
+        <button onClick={deleteNote}>Delete</button>
       </div>
-      <textarea onChange={(e)=> {setNote({...note, 'body':e.target.value})}} value={note?.body}></textarea>
+      <textarea onChange={(e) => { setNote({ ...note, 'body': e.target.value })}} value={note?.body}></textarea>
     </div>
   );
 }
